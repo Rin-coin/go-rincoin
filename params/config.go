8d6189486aebe24e10fac3135ddce5475c15a066
ysmreg <33682244+ysmreg@users.noreply.github.com>
2024-06-30 18:06:53 +0900

    Update config.go

--- params/config.go
+++ params/config.go
@@ -27,7 +27,7 @@ import (
 
 // Genesis hashes to enforce below configs on.
 var (
-	MainnetGenesisHash = common.HexToHash("0x3e561c6ff69c32acff813a010fece91262a0768828ac1f3b4900a043b0a5efcf")
+	MainnetGenesisHash = common.HexToHash("0x03501bd517dacb7837283e6cb7eceea34e1f2b7a24a1da55e425b79e268ff01f")
 	SepoliaGenesisHash = common.HexToHash("0x25a5cc106eea7138acab33231d7160d69cb777ee0c2c553fcddf5138993e6dd9")
 	RinkebyGenesisHash = common.HexToHash("0x6341fd3daf94b748c72ced5a5b26028f2474f5f00d824504e4fa37a75767e177")
 	GoerliGenesisHash  = common.HexToHash("0xbf7e331f7f7c1dd2e05159666b3bf8bc7a8a3a9eb1d518969eab529dd9b88c1a")
